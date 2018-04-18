4b6f625459e38964cb3278a51a5346a10bb296e5
tatianajiselle <tjexoxo@gmail.com>
2018-04-18 08:56:56 -0700

    Working session 5

--- session5/tx.py
+++ session5/tx.py
@@ -136,12 +136,17 @@ class Tx:
     def verify_input(self, input_index):
         '''Returns whether the input has a valid signature'''
         # get the relevant input
+        tx_in = self.tx_ins[input_index]
         # parse the point from the sec format (tx_in.sec_pubkey())
+        point = S256Point.parse(tx_in.sec_pubkey())
         # parse the signature from the der format (tx_in.der_signature())
+        signature = Signature.parse(tx_in.der_signature())
         # get the hash type from the input (tx_in.hash_type())
+        hash_type = tx_in.hash_type()
         # get the sig_hash (z)
+        z = self.sig_hash(input_index, hash_type)
         # use point.verify on the z and signature
-        raise NotImplementedError
+        return point.verify(z, signature)
 
     def sign_input(self, input_index, private_key, hash_type):
         '''Signs the input using the private key'''
